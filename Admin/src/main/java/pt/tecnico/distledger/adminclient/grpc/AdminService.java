7f5fc6024827d08b3eef9ee4c250b0074d1044bb
Sofia Pinho <sofia.pinho@tecnico.ulisboa.pt>
2023-03-08 14:27:19 +0000

    add method that builds a channel and stub, as well as individual methods for each remote operation of this service

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/grpc/AdminService.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/grpc/AdminService.java
@@ -1,8 +1,65 @@
 package pt.tecnico.distledger.adminclient.grpc;
 
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.ActivateRequest;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.ActivateResponse;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.DeactivateRequest;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.DeactivateResponse;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateRequest;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateResponse;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.function.Function;
+
 public class AdminService {
 
-    /* TODO: The gRPC client-side logic should be here.
-        This should include a method that builds a channel and stub,
-        as well as individual methods for each remote operation of this service. */
+    final String target;
+    final ManagedChannel channel;
+    static AdminServiceGrpc.AdminServiceBlockingStub blockingStub;
+
+    public AdminService(String host, int port) {
+        target = host + ":" + port;
+        channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        blockingStub = AdminServiceGrpc.newBlockingStub(channel);
+    }
+
+    public static void activate(String server) {
+        try {
+            //final ManagedChannel channel = ManagedChannelBuilder.forTarget(server).usePlaintext().build();
+            //AdminServiceGrpc.AdminServiceBlockingStub stub = AdminServiceGrpc.newBlockingStub(channel);
+
+            ActivateRequest activateRequest = ActivateRequest.getDefaultInstance();
+            ActivateResponse activateResponse = blockingStub.activate(activateRequest);
+            System.out.println("OK\n" + activateResponse.toString());
+
+            //channel.shutdown();
+        }
+        catch(StatusRuntimeException e) {
+            System.out.println("ERROR\n" + e.getStatus().getDescription());
+        }
+    }
+
+    public static void deactivate(String server) {
+        try {
+            DeactivateRequest deactivateRequest = DeactivateRequest.getDefaultInstance();
+            DeactivateResponse activateResponse = blockingStub.deactivate(deactivateRequest);
+            System.out.println("OK\n" + activateResponse.toString());
+        }
+        catch(StatusRuntimeException e) {
+            System.out.println("ERROR\n" + e.getStatus().getDescription());
+        }
+    }
+
+    public static void getLedgerState(String server) {
+        try {
+            getLedgerStateRequest request = getLedgerStateRequest.getDefaultInstance();
+            getLedgerStateResponse response = blockingStub.getLedgerState(request);
+            System.out.println("OK\n" + response.toString());
+        }
+        catch(StatusRuntimeException e) {
+            System.out.println("ERROR\n" + e.getStatus().getDescription());
+        }
+    }
 }
