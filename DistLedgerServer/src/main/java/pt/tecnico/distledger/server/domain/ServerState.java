961f7f1e891798f2d5fa841284f8392768a8b732
Margarida Bezerra <megui123455@gmail.com>
2023-03-08 15:25:26 +0000

    add getters, setters, operation and account related functions

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
@@ -1,18 +1,51 @@
 package pt.tecnico.distledger.server.domain;
 
+import java.util.*;
+import pt.tecnico.distledger.server.domain.operation.CreateOp;
+import pt.tecnico.distledger.server.domain.operation.DeleteOp;
+import pt.tecnico.distledger.server.domain.operation.TransferOp;
 import pt.tecnico.distledger.server.domain.operation.Operation;
+import pt.tecnico.distledger.server.domain.Account;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ServerState {
-    private List<Operation> ledger;
+
+	private List<Operation> ledger;
+	private Map<String, Account> accountsMap;
 
     public ServerState() {
         this.ledger = new ArrayList<>();
+        this.accountsMap = new HashMap<String, Account>();
     }
-
     /* TODO: Here should be declared all the server state attributes
          as well as the methods to access and interact with the state. */
+
+    public int balance(String id) {
+        Account account;
+        // grab Account by String id
+        account = accountsMap.get(id);
+        // check for errors
+        if (account == null){
+            // add specific exceptions
+            return -1;
+        }
+        return account.getBalance();
+    }
+
+    public Map<String, Account> getAccountsMap(){
+        return accountsMap;
+    }
+
+    public void addAccount(Account a){
+        accountsMap.put(a.getName(), a);
+    }
+
+    public List<Operation> getLedger(){
+        return ledger;
+    }
+
+    public void addOperation(Operation o){
+        ledger.add(o);
+    }
 
 }
