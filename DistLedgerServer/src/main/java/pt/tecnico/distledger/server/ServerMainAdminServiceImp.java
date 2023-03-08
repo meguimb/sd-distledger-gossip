f64abdee1d63ca40597efd51a0eea077fb56e66a
Sofia Pinho <sofia.pinho@tecnico.ulisboa.pt>
2023-03-08 17:25:14 +0000

    feat: complete methods in ServerMainAdminServiceImp

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMainAdminServiceImp.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMainAdminServiceImp.java
@@ -1,5 +1,6 @@
 package pt.tecnico.distledger.server;
 
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.DeactivateRequest;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.DeactivateResponse;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.ActivateRequest;
@@ -8,29 +9,78 @@ import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.GossipReques
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.GossipResponse;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateRequest;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateResponse;
+import pt.tecnico.distledger.server.domain.ServerState;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc;
+import pt.tecnico.distledger.server.domain.operation.Operation;
 import io.grpc.stub.StreamObserver;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.OperationType;
 import io.grpc.Status;
 
+import java.util.*;
+
 public class ServerMainAdminServiceImp extends AdminServiceGrpc.AdminServiceImplBase {
+  private ServerState state;
 
-    @Override
-    public void activate(ActivateRequest request, StreamObserver<ActivateResponse> responseObserver) {
-      // TODO
+  public AdminServiceImp(ServerState s) {
+    this.state = s;
+  }
+
+  @Override
+  public void activate(ActivateRequest request, StreamObserver<ActivateResponse> responseObserver) {
+    ActivateResponse response = ActivateResponse.newBuilder().build();
+    state.activate();
+
+    responseObserver.onNext(response);
+	  responseObserver.onCompleted();
+  }
+
+  @Override
+  public void deactivate(DeactivateRequest request, StreamObserver<DeactivateResponse> responseObserver) {
+    DeactivateResponse response = DeactivateResponse.newBuilder().build();
+    state.deactivate();
+
+    responseObserver.onNext(response);
+	  responseObserver.onCompleted();
+  }
+
+  @Override
+  public void gossip(GossipRequest request, StreamObserver<GossipResponse> responseObserver) {
+    // TODO - fase 3
+  }
+
+  @Override
+  public void getLedgerState(getLedgerStateRequest request, StreamObserver<getLedgerStateResponse> responseObserver) {
+    //getLedgerStateResponse response = getLedgerStateResponse.newBuilder().setLedgerState(state.getLedgerState()).build();
+    List<Operation> ledgerOps = state.getLedgerState();
+    List<DistLedgerCommonDefinitions.Operation> convertedOps = new ArrayList<>();
+
+    for(int i = 0; i < ledgerOps.size(); i++) {
+      Operation op = ledgerOps.get(i);
+      DistLedgerCommonDefinitions.Operation newOp;
+
+      if(op instanceof TransferOp) {
+        TransferOp transferOp = (TransferOp) op;
+        newOp = DistLedgerCommonDefinitions.Operation.newBuilder().setType(OperationType.OP_TRANSFER_TO).setUserId(transferOp.getAccount()).setDestUserId(transferOp.getDestAccount()).setAmount(transferOp.getAmount()).build();
+      }
+      else if(op instanceof CreateOp) {
+        CreateOp createOp = (CreateOp) op;
+        newOp = DistLedgerCommonDefinitions.Operation.newBuilder().setType(OperationType.OP_CREATE_ACCOUNT).setUserId(createOp.getAccount()).build();
+      }
+      else if(op instanceof DeleteOp) {
+        DeleteOp deleteOp = (DeleteOp) op;
+        newOp = DistLedgerCommonDefinitions.Operation.newBuilder().setType(OperationType.OP_DELETE_ACCOUNT).setUserId(deleteOp.getAccount()).build();
+      }
+      else {
+        newOp = DistLedgerCommonDefinitions.Operation.newBuilder().setType(OperationType.OP_UNSPECIFIED).build();
+      }
+
+      convertedOps.add(newOp);
     }
 
-    @Override
-    public void deactivate(DeactivateRequest request, StreamObserver<DeactivateResponse> responseObserver) {
-      // TODO
-    }
+    DistLedgerCommonDefinitions.LedgerState ledgerState = DistLedgerCommonDefinitions.LedgerState.newBuilder().addAllOperations(convertedOps).build();
+    getLedgerStateResponse response = getLedgerStateResponse.newBuilder().setLedgerState(ledgerState).build();
 
-    @Override
-    public void gossip(GossipRequest request, StreamObserver<GossipResponse> responseObserver) {
-      // TODO
-    }
-
-    @Override
-    public void getLedgerState(getLedgerStateRequest request, StreamObserver<getLedgerStateResponse> responseObserver) {
-      // TODO
-    }
+    responseObserver.onNext(response);
+	  responseObserver.onCompleted();
+  }
 }
