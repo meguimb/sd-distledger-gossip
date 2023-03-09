c36da62a5626676095d325ce17820abce515c5e8
Vasco <vascobrito01@tecnico.ulsiboa.pt>
2023-03-09 18:46:04 +0000

    feat: implemented debug functionality for AdminService and UserService

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/grpc/AdminService.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/grpc/AdminService.java
@@ -18,6 +18,7 @@ public class AdminService {
     final String target;
     final ManagedChannel channel;
     static AdminServiceGrpc.AdminServiceBlockingStub blockingStub;
+    private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
 
     public AdminService(String host, int port) {
         target = host + ":" + port;
@@ -26,35 +27,50 @@ public class AdminService {
     }
 
     public static void activate(String server) {
+        debug("Attempting to activate server");
         try {
             ActivateRequest activateRequest = ActivateRequest.getDefaultInstance();
             ActivateResponse activateResponse = blockingStub.activate(activateRequest);
             System.out.println("OK\n" + activateResponse.toString());
+            debug("Server activated successfully!");
         }
         catch(StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to activate server!");
         }
     }
 
     public static void deactivate(String server) {
+        debug("Attempting to deactivate server")
         try {
             DeactivateRequest deactivateRequest = DeactivateRequest.getDefaultInstance();
             DeactivateResponse activateResponse = blockingStub.deactivate(deactivateRequest);
             System.out.println("OK\n" + activateResponse.toString());
+            debug("Server deactivated successfully!");
         }
         catch(StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to deactivate server!");
         }
     }
 
     public static void getLedgerState(String server) {
+        debug("Attempting to get ledger state");
         try {
             getLedgerStateRequest request = getLedgerStateRequest.getDefaultInstance();
             getLedgerStateResponse response = blockingStub.getLedgerState(request);
             System.out.println("OK\n" + response.toString());
+            debug("Ledger state retrieved successfully!");
         }
         catch(StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to get ledger state!");
+        }
+    }
+
+    private static void debug(String message) {
+        if (DEBUG_FLAG) {
+            System.err.println(message);
         }
     }
 
--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -19,6 +19,7 @@ public class UserService {
     final String target;
     final ManagedChannel channel;
     static UserServiceGrpc.UserServiceBlockingStub stub;
+    private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
 
     public UserService(String host, int port) {
         target = host + ":" + port;
@@ -27,46 +28,64 @@ public class UserService {
     }
     
     public static void createAccount(String server, String username) {
+        debug("Attempting to create account for user " + username + "...");
         try {
             CreateAccountRequest createAccountRequest = CreateAccountRequest.newBuilder().setUserId(username).build();
             CreateAccountResponse createAccountResponse = stub.createAccount(createAccountRequest);
             System.out.println("OK\n" + createAccountResponse.toString());
+            debug("Account created successfully!");
         }
         catch (StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to create account!");
         }
     }
 
     public static void deleteAccount(String server, String username) {
+        debug("Attempting to delete " + username + "'s account...");
         try {
             DeleteAccountRequest deleteAccountRequest = DeleteAccountRequest.newBuilder().setUserId(username).build();
             DeleteAccountResponse deleteAccountResponse = stub.deleteAccount(deleteAccountRequest);
             System.out.println("OK\n" + deleteAccountResponse.toString());
+            debug("Account deleted successfully!");
         }
         catch (StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to delete account!");
         }
     }
 
     public static void balance(String server, String username) {
+        debug("Attempting to get " + username + "'s balance...");
         try {
             BalanceRequest balanceRequest = BalanceRequest.newBuilder().setUserId(username).build();
             BalanceResponse balanceResponse = stub.balance(balanceRequest);
             System.out.println("OK\n" + balanceResponse.toString());
+            debug("Balance retrieved successfully!");
         }
         catch (StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to get balance!");
         }
     }
 
     public static void transferTo(String server, String from, String to, Integer amount) {
+        debug("Attempting to transfer " + amount + " from " + from + " to " + to + "...");
         try {
             TransferToRequest transferToRequest = TransferToRequest.newBuilder().setAccountFrom(from).setAccountTo(to).setAmount(amount).build();
             TransferToResponse transferToResponse = stub.transferTo(transferToRequest);
             System.out.println("OK\n" + transferToResponse.toString());
+            debug("Transfer successful!");
         }
         catch (StatusRuntimeException e) {
             System.out.println("ERROR\n" + e.getStatus().getDescription());
+            debug("Failed to transfer!");
+        }
+    }
+
+    private static void debug(String message) {
+        if (DEBUG_FLAG) {
+            System.err.println(message);
         }
     }
 
