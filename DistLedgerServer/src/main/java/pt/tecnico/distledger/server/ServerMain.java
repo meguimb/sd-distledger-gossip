3985260f5dee725af566d2753debc957c004637c
Margarida Bezerra <margarida.bezerra@tecnico.ulisboa.pt>
2023-03-09 16:25:57 +0000

    clean code, fix comments and add prints to ServerMain

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -9,44 +9,10 @@ import pt.tecnico.distledger.server.ServerMainAdminServiceImp;
 import java.io.IOException;
 
 public class ServerMain {
-	/*
-	 * A lista de operações de escrita aceites, também chamada ledger. Inclui operações 
-	 * de 3 tipos: criar conta, remover conta, transferir moeda entre contas. Inicialmente 
-	 * está vazia e vai crescendo à medida que lhe são acrescentadas novas operações. 
-	 * Um mapa de contas com informação sobre as contas ativas neste momento e o respetivo 
-	 * saldo. Esta estrutura descreve o estado que resulta da execução ordenada de todas as 
-	 * operações atualmente na ledger e que, na 3ª parte, já estão estáveis (stable updates, 
-	 * segundo a terminologia do gossip architecture, o modelo de replicação que vamos usar 
-	 * nessa parte do projeto). Sempre que uma nova operação é adicionada à ledger e estabiliza
-	 *  (novamente, isto só é relevante na 3ª parte), o estado do mapa de contas deve ser 
-	 * atualizado para refletir essa operação.
-	 */
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        // Nesta fase o serviço é prestado por um único servidor, que aceita 
-        // pedidos num endereço/porto fixo que é conhecido de antemão por todos os clientes.
-
-        // Cada servidor exporta múltiplas interfaces. Cada interface está pensada para 
-        // expor operações a cada tipo de cliente (utilizadores e administradores). Para 
-        // além dessas, os servidores exportam uma terceira interface pensada para ser 
-        //invocada por outros servidores (no caso em que os servidores estão replicados, 
-        // e necessitam de comunicar entre si).
-
-        // O servidor deve ser lançado a partir da pasta Server, recebendo como argumentos
-        // o porto e o seu qualificador ('A', 'B', etc.). Na fase 1, o qualificador passado 
-        // é ignorado.
-
-        // Por exemplo, um servidor primário pode ser lançado da seguinte forma a partir da
-        // pasta Server ($ representa a shell do sistema operativo):
-
         // $ mvn exec:java -Dexec.args="2001 A"
-        System.out.printf("Current Server running is: %s", ServerMain.class.getSimpleName());
-
-		// receive and print arguments
-		System.out.printf("Received %d arguments%n", args.length);
-		for (int i = 0; i < args.length; i++) {
-			System.out.printf("arg[%d] = %s%n", i, args[i]);
-		}
+        System.out.printf("Current Server running is: %s\n", ServerMain.class.getSimpleName());
 
 		// check arguments
 		if (args.length < 2) {
@@ -55,30 +21,22 @@ public class ServerMain {
 			return;
 		}
 
+		// get port and ignore qualificator for now
 		final int port = Integer.parseInt(args[0]);
+		char qualificator = args[1].charAt(0);
+
+		// create server state
 		ServerState serverState = new ServerState();
+
+		// create user and admin service implementations for main server
 		final BindableService impUser = new ServerMainUserServiceImp(serverState);
 		final BindableService impAdmin = new ServerMainAdminServiceImp(serverState);
+
+		// create server with both services and start it
 		Server server = ServerBuilder.forPort(port).addService(impUser).addService(impAdmin).build();
 		server.start();
-		System.out.println("Server started");
+		System.out.printf("Server %c started at port %d\n", qualificator, port);
 		server.awaitTermination();
-
-        /* 
-		final BindableService impl = new TTTServiceImpl();
-
-		// Create a new server to listen on port
-		Server server = ServerBuilder.forPort(port).addService(impl).build();
-
-		// Start the server
-		server.start();
-
-		// Server threads are running in the background.
-		System.out.println("Server started");
-
-		// Do not exit the main thread. Wait until server is terminated.
-		server.awaitTermination();
-        */
     }
 
 }
